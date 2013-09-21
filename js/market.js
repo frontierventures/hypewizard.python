3e3c922cc809c5773ff7309a17937001858db955
Konstantin Yurchenko <konstantin.k.yurchenko@gmail.com>
2013-09-21 01:01:05 -0600

    Fixed

--- js/market.js
+++ js/market.js
@@ -39,27 +39,27 @@ function build_grid(kind) {
 };
 function add_ask_to_market(rule, ask) { 
     var action_url = '../feature_disabled?reason=not_authorized';
-    var action_cell = '<td style="text-align:center"><a href="' + action_url + '">Engage Client</a></td>';
+    var action_cell = '<div style="text-align:center"><a href="' + action_url + '">Engage Client</a></div>';
 
     if (rule == 'none') {
         action_url = '../process_ask?action=engage&id=' + ask.id; 
-        action_cell = '<td style="text-align:center"><a href="' + action_url + '">Engage Client</a></td>';
+        action_cell = '<div style="text-align:center"><a href="' + action_url + '">Engage Client</a></div>';
         if (ask.rule == 'none') {
             action_url = '../process_ask?action=withdraw&id=' + ask.id; 
-            action_cell = '<td style="text-align:center"><a href="' + action_url + '">Withdraw</a></td>';
+            action_cell = '<div style="text-align:center"><a href="' + action_url + '">Withdraw</a></div>';
         }
     }
 
     $('.asks').append(
-            '<tr>' + 
-            '<td style="text-align:center">' + ask.niche + '</td>' +
-            '<td style="text-align:center"><a href="https://twitter.com/' + ask.twitter_name + '">' + ask.twitter_name + '</td>' +
-            '<td style="text-align:center"><a href="https://twitter.com/' + ask.twitter_name + '/status/' + ask.twitter_status_id + '">' + ask.twitter_status_id + '</td>' +
-            '<td style="text-align:center">' + ask.campaign_type + '</td>' +
-            '<td style="text-align:center">' + ask.target + ' of ' + ask.goal + '</td>' +
-            '<td style="text-align:center">' + ask.cost + '</td>' +
+            '<div class="block">' + 
+            '<div style="text-align:center">' + ask.niche + '</div>' +
+            '<div style="text-align:center"><a href="https://twitter.com/' + ask.twitter_name + '">' + ask.twitter_name + '</div>' +
+            '<div style="text-align:center"><a href="https://twitter.com/' + ask.twitter_name + '/status/' + ask.twitter_status_id + '">' + ask.twitter_status_id + '</div>' +
+            '<div style="text-align:center">' + ask.campaign_type + '</div>' +
+            '<div style="text-align:center">' + ask.target + ' of ' + ask.goal + '</div>' +
+            '<div style="text-align:center">' + ask.cost + '</div>' +
             action_cell + 
-            '</tr>');
+            '</div>');
 };
 function add_bid_to_market(rule, bid) { 
     var user = {};
@@ -75,23 +75,23 @@ function add_bid_to_market(rule, bid) {
     });
     
     var action_url = '../feature_disabled?reason=not_authorized';
-    var action_cell = '<td style="text-align:center"><a href="' + action_url + '">Engage Client</a></td>';
+    var action_cell = '<div style="text-align:center"><a href="' + action_url + '">Engage Client</a></div>';
 
     if (rule == 'none') {
         action_url = '../process_bid?action=engage&id=' + bid.id; 
-        action_cell = '<td style="text-align:center"><a href="' + action_url + '">Engage Client</a></td>';
+        action_cell = '<div style="text-align:center"><a href="' + action_url + '">Engage Client</a></div>';
         if (bid.rule == 'none') {
             action_url = '../process_bid?action=withdraw&id=' + bid.id; 
-            action_cell =  '<td style="text-align:center"><a href="' + action_url + '">Withdraw</a></td>';
+            action_cell =  '<div style="text-align:center"><a href="' + action_url + '">Withdraw</a></div>';
         }
     }
     $('.bids').append(
-            '<tr>' + 
-            '<td style="text-align:center">' + bid.niche + '</td>' +
-            '<td style="text-align:center"><a href="http://twitter.com/' + bid.twitter_name + '">' + bid.twitter_name + '</td>' +
-            '<td style="text-align:center"> (S: ' + user.statuses_count + ', F: ' + user.followers_count + ')</td>' +
-            '<td style="text-align:center">' + bid.campaign_type + '</td>' +
-            '<td style="text-align:center">' + bid.cost + '</td>' +
+            '<div class="block">' + 
+            '<div style="text-align:center">' + bid.niche + '</div>' +
+            '<div style="text-align:center"><a href="http://twitter.com/' + bid.twitter_name + '">' + bid.twitter_name + '</div>' +
+            '<div style="text-align:center"> (S: ' + user.statuses_count + ', F: ' + user.followers_count + ')</div>' +
+            '<div style="text-align:center">' + bid.campaign_type + '</div>' +
+            '<div style="text-align:center">' + bid.cost + '</div>' +
             action_cell + 
-            '</tr>');
+            '</div>');
 };
